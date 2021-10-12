89849b9fdeb2f16f3fa868ed262107c3ca475276
Simon Rit <simon.rit@creatis.insa-lyon.fr>
2021-10-12 09:57:20 +0200

    BUG: fix regular expressions for artihmetic with more than 2 operands

--- ProcessForbildPhantomFile.py
+++ ProcessForbildPhantomFile.py
@@ -2,14 +2,14 @@ import re
 import os
 import numpy as np
 
-f = open('Thorax', 'w')
-with os.popen("gcc -E -x c++ ThoraxPhantom.pha") as pipe:
+f = open('Head', 'w')
+with os.popen("gcc -E -x c++ HeadPhantom.pha") as pipe:
     for line in pipe:
         while True:
             (line, n1) = re.subn(r'cos\(([\-+*/0-9\. ]*)\)', lambda m: "%+f" % (np.cos(np.deg2rad(eval(m.group(1))))), line)   # cos
             (line, n2) = re.subn(r'sin\(([\-+*/0-9\. ]*)\)', lambda m: "%+f" % (np.sin(np.deg2rad(eval(m.group(1))))), line)   # sin
             (line, n3) = re.subn(r'sqrt\(([\-+*/0-9\. ]*)\)', lambda m: "%+f" % (np.sqrt(eval(m.group(1)))), line)             # SQRT
-            (line, n4) = re.subn(r'([\[+\-]?[0-9\.]+ *[*+\-/]+ *[+\-]?[0-9\.]+)', lambda m: "%+f" % (eval(m.group(1))), line)   # Arithmetic
+            (line, n4) = re.subn(r'([\[+\-]?[0-9\.]+( *[*+\-/]+ *[+\-]?[0-9\.]+)+)', lambda m: "%+f" % (eval(m.group(1))), line)   # Arithmetic
             (line, n5) = re.subn(r'\( *([\[+-]?[0-9\.]*) *\)', r'\1', line)                                                     # Number in () 
             (line, n6) = re.subn(r'[+] *[\-]', '-', line)                                                                      # +- => -
             (line, n7) = re.subn(r'[\-] *[+]', '-', line)                                                                      # -+ => -
